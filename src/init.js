c4cfb7b054cc36936faa9a2ffa179de4c68b8706
sunyu@troila.com <sunyu@troila.com>
2020-06-17 12:12:22 +0800

    添加git

--- src/init.js
+++ src/init.js
@@ -1,11 +1,40 @@
 const chalk = require('chalk')
 const symbol = require('log-symbols')
+const async = require("async")
 const util = require('./util')
 const fileName = 'package.json'
 
-const callback = () =>{
-	console.log('安装完成')
+const initPluginGit = (answer, name) => {
+	let stepFunc = [
+		(callback) => {
+			util.loadCmd(
+				`cd ${name} && ${answer['yarn'] ? 'yarn' :  'npm i'}`,
+				()=> callback(null)
+			)
+		},
+		(callback) => {
+			if(answer['git']) {
+				console.log(chalk.blue('==================='))
+				console.log('初始化git中...')
+				callback(null)
+			}
+		},
+		(callback) => {
+			util.loadCmd(`cd ${name} && git init`, ()=>callback(null))
+		},
+		(callback) => {
+			util.loadCmd(`cd ${name} && git add .`, ()=> callback(null))
+		},
+		(callback) => {
+			util.loadCmd(`cd ${name} && git commit -m "初始化"`, ()=> callback(null))
+		},
+		(callback) => {
+			console.log(symbol.success, chalk.green('初始化git完成'))
+		}
+	]
+	async.series(stepFunc)
 }
+
 
 const init = (answer, name) => {
 	let obj = {
@@ -17,10 +46,7 @@ const init = (answer, name) => {
 			symbol.success,
 			chalk.green('更新'+ fileName + '完成')
 		)
-		util.loadCmd(
-			`cd ${name} && ${answer['yarn'] ? 'yarn' :  'npm i'}`,
-			callback
-		)
+		initPluginGit(answer, name)
 	}).catch(()=>{
 		console.log(
 			symbol.error,
